0ce1ca148c1b8e4313686e646bf4a9ca68934665
Spartwo <sparthesecond@gmail.com>
2025-03-14 07:13:20 +0000

    PrivateKey

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ sudo apt update
 
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
-
+sudo apt-get install openssl
 sudo npm install -g pm2
 
 pm2 stop mathapp
@@ -23,7 +23,11 @@ fi
 
 
 npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+
 
 pm2 start ./bin/www --name mathapp
 
 pm2 save
+
