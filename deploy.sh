fc594772a839ee884a8a7e4d781a851d798b6ff8
Spartwo <sparthesecond@gmail.com>
2025-03-14 07:57:00 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-node -v
-npm -v
 
 # Remove any old versions
 sudo apt remove nodejs npm
@@ -15,7 +13,7 @@ pm2 stop mathapp
 
 # Check if SoloCA directory exists
 if [ -d "SoloCA" ]; then
-  cd SoloCA
+  cd SoloCA/
 else
   echo "SoloCA directory does not exist!"
   exit 1
@@ -25,6 +23,8 @@ fi
 npm install
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
+
+openssl req -x509 -newkey rsa:4096 -keyout privatekey.pem -out server.crt -days 10000 -nodes -subj "/C=IE/ST=Leinster/L=Dublin/O=National College of Ireland/OU=School of Computing/CN=ncirl.ie"
 
 
 pm2 start ./bin/www --name mathapp
